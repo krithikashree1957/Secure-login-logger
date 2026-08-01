be496fcb58dd82e6c17a6b916a9d4e6e45cbc644
Krithika shree K <krithikashree.k2024@vitstudent.ac.in>
2026-08-01 16:55:06 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/krithika/secure_login_logger/SecureLoginLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.krithika.secure_login_logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureLoginLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureLoginLoggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krithika/secure_login_logger/SecureLoginLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krithika.secure_login_logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureLoginLoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
