09c4acae9cf6959f9f3766ce8b999992f92e9f4d
Krithika shree K <krithikashree.k2024@vitstudent.ac.in>
2026-08-01 17:14:36 +0530

    Configure UTc timezone

--- src/main/java/com/krithika/secure_login_logger/SecureLoginLoggerApplication.java
+++ src/main/java/com/krithika/secure_login_logger/SecureLoginLoggerApplication.java
@@ -1,4 +1,5 @@
 package com.krithika.secure_login_logger;
+import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecureLoginLoggerApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(SecureLoginLoggerApplication.class, args);
 	}
 
