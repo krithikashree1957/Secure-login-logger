27782e53b9e41b352a95cded45c98999f406ed12
Krithika shree K <krithikashree.k2024@vitstudent.ac.in>
2026-08-02 14:57:56 +0530

    Accept registration requests using RequestBody DTO

--- src/main/java/com/krithika/secure_login_logger/controller/AuthController.java
+++ src/main/java/com/krithika/secure_login_logger/controller/AuthController.java
@@ -2,6 +2,8 @@ package com.krithika.secure_login_logger.controller;
 import org.springframework.web.bind.annotation.RestController;
 import com.krithika.secure_login_logger.service.AuthService;
 import org.springframework.web.bind.annotation.PostMapping;
+import com.krithika.secure_login_logger.dto.RegisterRequest;
+import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class AuthController {
     private final AuthService authService;
@@ -9,8 +11,10 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
-    @PostMapping("/register")//Whenever someone sends a POST request to /register, run the method below.
-    public String register() {
-        return "Register endpoint is working!";
-}
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+
+        return "Received registration request for " + request.getEmail();
+
+    }
 }
--- src/main/java/com/krithika/secure_login_logger/dto/RegisterRequest.java
+++ src/main/java/com/krithika/secure_login_logger/dto/RegisterRequest.java
@@ -1,5 +1,29 @@
 package com.krithika.secure_login_logger.dto;
+//DTO (Data Transfer Object).
+//DTO represents the data that will be sent from the client to the server when a user registers for an account. 
+// It contains two fields: email and password, along with their corresponding getters and setters.
 
 public class RegisterRequest {
 
+    private String email;
+    private String password;
+
+    public RegisterRequest() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
